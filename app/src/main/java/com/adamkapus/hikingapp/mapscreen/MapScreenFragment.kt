34db555e168aca5d6a0fc6e4d5c02203fb588941
adamkapus <kapusadam03@gmail.com>
2022-10-03 23:12:56 +0200

    Add opening of .gpx files from device

--- app/src/main/java/com/adamkapus/hikingapp/mapscreen/MapScreenFragment.kt
+++ app/src/main/java/com/adamkapus/hikingapp/mapscreen/MapScreenFragment.kt
@@ -3,11 +3,17 @@ package com.adamkapus.hikingapp.mapscreen
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -19,6 +25,8 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MapScreenFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentMapScreenBinding
@@ -29,9 +37,22 @@ class MapScreenFragment : Fragment(), OnMapReadyCallback {
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
 
+        const val PICK_GPX_FILE = 101
+
         private const val DEFAULT_LOCATION_LAT = 47.4733057775952
         private const val DEFAULT_LOCATION_LNG = 19.059724793021967
     }
+
+    private val startForGPXFileResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                intent?.data?.also { uri ->
+                    openGPXFile(uri)
+                }
+            }
+        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,7 +69,7 @@ class MapScreenFragment : Fragment(), OnMapReadyCallback {
         mapFragment?.getMapAsync(this)
 
         binding.test.setOnClickListener {
-            displayGpx()
+            askForGpxFile()
         }
     }
 
@@ -61,8 +82,23 @@ class MapScreenFragment : Fragment(), OnMapReadyCallback {
         enableMyLocation()
     }
 
-    private fun displayGpx() {
+    private fun askForGpxFile() {
 
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/gpx+xml"
+        }
+        startForGPXFileResult.launch(intent)
+    }
+
+    private fun openGPXFile(uri: Uri) {
+        Log.d("PLS", uri.toString())
+        val contentResolver = context?.contentResolver
+        if(contentResolver != null) {
+            contentResolver.openInputStream(uri)?.use { inputStream ->
+
+            }
+        }
 
     }
 
